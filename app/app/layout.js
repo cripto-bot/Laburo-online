fb81a0273852045020463185e9be8216def6a3ef
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-05-27 09:32:22 -0300

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,9 @@
+// Este es el layout raíz y es requerido.
+// Puedes personalizarlo más adelante.
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
