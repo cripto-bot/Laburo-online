9b699a1910d0a020bb9639bd97ac0b781fd29233
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-05-27 11:06:49 -0300

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Bienvenido a Laburo en Línea</h1>
-      <p>El API para crear trabajos debería estar en /api/crearTrabajo</p>
-    </div>
-  );
-}
