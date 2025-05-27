f1250fb142df9c35ab45c32afd06bfc618335825
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-05-27 11:06:14 -0300

    Delete app/app directory

--- app/app/layout.js
+++
@@ -1,9 +0,0 @@
-// Este es el layout raíz y es requerido.
-// Puedes personalizarlo más adelante.
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body>{children}</body>
-    </html>
-  )
-}
