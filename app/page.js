eb434df43f12326a71e167b1a016faa35dfff33d
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-05-27 09:31:34 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido a Laburo en Línea</h1>
+      <p>El API para crear trabajos debería estar en /api/crearTrabajo</p>
+    </div>
+  );
+}
